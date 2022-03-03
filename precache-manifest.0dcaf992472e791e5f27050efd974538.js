26eaba6b888aa8d9ca421f4fc85446327caff2b6
henryburkhardt <henrywb04@gmail.com>
2022-03-03 12:20:21 -0600

    Updates

---
+++ precache-manifest.0dcaf992472e791e5f27050efd974538.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2816233e87fbbfadf807d0771f645e5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b84bb4c035f9b60ea3a6",
+    "url": "/static/css/main.8594c20e.chunk.css"
+  },
+  {
+    "revision": "1416b72c50c363d73660",
+    "url": "/static/js/2.6f0d2da3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f0d2da3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84bb4c035f9b60ea3a6",
+    "url": "/static/js/main.8bc10191.chunk.js"
+  },
+  {
+    "revision": "eb2a898cb490e53eed19",
+    "url": "/static/js/runtime-main.ba3ba41b.js"
+  },
+  {
+    "revision": "8ca0cab67aa18c9010625b7545833f90",
+    "url": "/static/media/DOS.8ca0cab6.png"
+  }
+]);
